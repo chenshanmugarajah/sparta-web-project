2bb3bb05ea026ca9f7a28306f0801df4f8edea00
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-08-16 19:21:37 +0100

    made file changes

---
+++ assets/js/cursor.js
@@ -0,0 +1,12 @@
+console.log("Cursor JS loaded")
+
+// window.onload = 
+
+var ball = document.getElementById('ball')
+
+document.addEventListener('mousemove', function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    ball.style.left = x + "px";
+    ball.style.top = y + "px";
+})
---
+++ assets/js/nasa-fetch.js
@@ -0,0 +1,39 @@
+
+var randomNumberBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var searchNasa = (query, targetDiv, page) => {
+
+    fetch("https://images-api.nasa.gov/search?q=" + query)
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        response.json().then(function(data) {
+            displayData(data.collection.items, targetDiv, page)
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    })
+
+}
+
+var displayData = (data, targetDiv, page) => {
+    var rand = randomNumberBetween(20, 50)
+    var selectedData = data[rand].data[0]
+    var nasaId = selectedData.nasa_id
+    var title = selectedData.title;
+    var desc = selectedData.description;
+    var date_created = selectedData.date_created.split("T");
+    var img = data[rand].links[0].href
+
+    if (page == "homepage") {
+        document.querySelector(targetDiv).innerHTML = `<h1>${nasaId}: ${title}<h1/><h3>${desc}<h3/><img src=${img} class="modal-img"><p>Date created: ${date_created[0]}<p/>`;
+    }
+}
